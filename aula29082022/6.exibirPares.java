d2a991ed0d5257255458341e7e3d3cbaf2f702f0
Fernanda Souza <fernanda.fernandes10@fatec.sp.gov.br>
2022-09-12 10:57:05 -0300

    Create 6.exibirPares.java

---
+++ aula29082022/6.exibirPares.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+	
+class Main {
+  public static void main(String[] args) {
+		Scanner leitura = new Scanner(System.in);
+		//desenvolva um programa que recebe dois números e apresente os números pares desse intervalo
+		int num1,num2;
+    System.out.println("Digite o primeiro número: ");
+		num1 = leitura.nextInt();
+		System.out.println("Digite o segundo número: ");
+		num2 = leitura.nextInt();
+
+		if(num1 < num2){
+			for(int i = num1; i < num2; i++){
+				
+			}
+		} else 
+			
+		for(int i = 0; i < 10; i++)
+    System.out.println(i + " Bom dia");
+		//System.out.println(i);
+  	}
+  }
+}
