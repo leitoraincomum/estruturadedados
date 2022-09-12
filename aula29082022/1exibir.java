7cf3777e04122a18922e443fdb2f0db297460871
Fernanda Souza <fernanda.fernandes10@fatec.sp.gov.br>
2022-09-12 10:26:58 -0300

    Create 1exibir.java

---
+++ aula29082022/1exibir.java
@@ -0,0 +1,11 @@
+class Main {
+  public static void main(String[] args) {
+//loop que mostra números menores que 10
+		int x = 1;		
+		while(x <10){
+			System.out.println(x);
+			x++;
+		}
+		
+  }
+}
